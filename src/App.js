f98e5fc322358c934e8202f2afe7a8a96fd4399d
Phuong Le <meoleteds@yahoo.com>
2019-03-29 12:06:22 -0400

    changed text in feature branch

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React !!
+            New from feature branch!
           </a>
         </header>
       </div>
